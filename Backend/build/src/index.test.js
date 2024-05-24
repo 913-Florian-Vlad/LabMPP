0d0ed286f9ac8707a2c64d01ae91a94b2cb91c63
Florian-Vlad <91673243+913-Florian-Vlad@users.noreply.github.com>
2024-05-24 09:42:02 +0300

    Delete Backend directory

--- Backend/build/src/index.test.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const globals_1 = require("@jest/globals");
-(0, globals_1.describe)('index', () => {
-    (0, globals_1.test)('should work', () => {
-        (0, globals_1.expect)(true).toBe(true);
-    });
-});
--- Backend/src/index.test.ts
+++
@@ -1,7 +0,0 @@
-import {describe, expect, test} from '@jest/globals';
-
-describe('index', () => {
-  test('should work', () => {
-    expect(true).toBe(true);
-  });
-});
